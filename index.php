c71853e08519528f1e9e365c1b7a34e5106cebca
craignelson <craig@busybit.co.uk>
2020-02-08 13:06:13 +0000

    images and css

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>CSS Grid</title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="container">
+  <div class="vertical"><img src="/img/1.jpg" style="width:100%"></div>
+  <div><img src="/img/2.jpg"style="width:100%"></div>
+  <div><img src="/img/3.jpg"style="width:100%"></div>
+  <div class="big" ><img src="/img/4.jpg"style="width:100%"></div>
+  <div><img src="/img/5.png"style="width:100%"></div>
+  <div><img src="/img/6.jpg"style="width:100%"></div>
+  <div class="vertical"><img  src="/img/7.jpg"style="width:100%"></div>
+  <div><img src="/img/8.jpg"style="width:100%"></div>
+  <div><img src="/img/9.jpg"style="width:100%"></div>
+  <div class="horizontal"><img src="/img/10.jpg"style="width:100%"></div>
+  <div><img src="/img/11.jpg"style="width:100%"></div>
+</div>
+</body>
+</html>
+
+
